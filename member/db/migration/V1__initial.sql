26716592726293295c7220a109a5fe71d94d41ce
jhonatapers <jhonatapers@gmail.com>
2025-09-28 14:30:36 -0300

    feat: update systems and members tables with new constraints and columns

--- member/db/migration/V1__initial.sql
+++ member/db/migration/V1__initial.sql
@@ -1,22 +1,23 @@
 CREATE TABLE systems (
-    system VARCHAR(30) NOT NULL CHECK (system IN ('DRIVE','MEMBER')),
+    system VARCHAR(60) NOT NULL CHECK (system IN ('DRIVE','MEMBER')),
     PRIMARY KEY (system)
 );
 
 CREATE TABLE members (
-    id VARCHAR(255) NOT NULL,
+    id UUID NOT NULL,
     active BOOLEAN NOT NULL,
     created_at TIMESTAMP(6) WITH TIME ZONE,
     updated_at TIMESTAMP(6) WITH TIME ZONE,
     email VARCHAR(255),
     nickname VARCHAR(255),
     username VARCHAR(255),
+    synchronized_version BIGINT NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE member_system (
-    member_id VARCHAR(255) NOT NULL,
-    system_id VARCHAR(255) NOT NULL,
+    member_id UUID NOT NULL,
+    system_id VARCHAR(60) NOT NULL,
     PRIMARY KEY (member_id, system_id)
 );
 
@@ -25,3 +26,7 @@ ALTER TABLE member_system
 
 ALTER TABLE member_system
     ADD CONSTRAINT fk_member_system_member_id FOREIGN KEY (member_id) REFERENCES members(id);
+
+INSERT INTO systems (system) VALUES 
+    ('DRIVE'),
+    ('MEMBER');
